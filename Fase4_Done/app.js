4c66861956eb16a8517ef5689b2881fef899585d
Jorge Diaz <jorgeldra@gmail.com>
2017-06-16 23:40:11 +0200

    feat(fase4): añade solución para componetizar

---
+++ Fase4_Done/app.js
@@ -0,0 +1,8 @@
+(function() {
+    function RelojController(){
+        //now this is more clean
+    }
+    angular
+        .module("RelojApp",[])
+        .controller("RelojController", RelojController);
+})();
